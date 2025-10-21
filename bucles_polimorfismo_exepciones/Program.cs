6ae1df4854552f2524eb5999e26d6259cb9c43eb
jose rendon gomez <medinajordan152@gmail.com>
2025-10-20 22:09:51 -0500

    Actualizar código de bucles_polimorfismo_exepciones con cambios finales

--- bucles_polimorfismo_exepciones/Program.cs
+++ bucles_polimorfismo_exepciones/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var circ = new Circulo();
+circ.Radio = 20;
+var result_area = circ.Area();
+var result_perin = circ.Perimetro();
+
+Console.WriteLine($"Area es: {result_area} y el perimtro es: {result_perin} el nuevo radio es: {circ.Add10()}");
+
+int prev = 0, next = 1, sum;
+for (int i = 0; i < 10; i++)
+
+{
+    Console.WriteLine(prev);
+    sum = prev + next;
+    prev = next;
+    next = sum;
+
+}
+
+try
+{
+    Console.WriteLine("numero a dividir: ");
+    double n = double.Parse(Console.ReadLine());
+    Console.WriteLine("Divisor: ");
+    double divisor = double.Parse(Console.ReadLine());
+    if (divisor == 0)
+
+    {
+        throw new DivideByZeroException();
+    }
+    Console.WriteLine($"Resultado: {n / divisor} ");
+}
+catch (DivideByZeroException )
+{
+     Console.WriteLine("No se puede dividir por cero ");
+}
