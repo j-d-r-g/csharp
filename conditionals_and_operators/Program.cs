5518679b55bc14669c3e2fe4f82da151f951420b
jose rendon gomez <medinajordan152@gmail.com>
2025-10-20 21:30:06 -0500

    Guardar cambios antes de cambiar de branch

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,15 @@
+﻿/*conditionals
+Console.WriteLine("ingrese un numero: ");
+int num = int.Parse(Console.ReadLine() );
+if (num > 0) Console.WriteLine("positivo");
+else if (num < 0) Console.WriteLine("negativo");
+else Console.WriteLine("Es cero");
+*/
+
+/*operators*/
+Console.WriteLine("ingrese el primer numero: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("ingrese el segundo numero: ");
+double b = double.Parse(Console.ReadLine());
+const string Indeterminado = "indeterminado";
+Console.WriteLine($"suma: {a + b}, resta: {a - b}, multiplicacion: {a * b}, division: {(b!= 0 ? a/b : Indeterminado)}");
